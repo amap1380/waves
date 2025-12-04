e4f05e7d47efcf6083b32905d7578b975ea80bb2
amirmohammad ahmadpour <58068218+amap1380@users.noreply.github.com>
2025-12-04 14:36:22 +0330

    Update level_manager.gd

--- scripts/level_manager.gd
+++ scripts/level_manager.gd
@@ -4,8 +4,11 @@ extends Node
 
 var current_level: int = 1
 var level_unlocked: int = 1
-var max_level: int = 3
+var max_level: int = 0
 
 func _unlock_level(level_to_unlock: int) -> void:
 	if level_to_unlock > level_unlocked:
 		pass
+
+func _ready() -> void:
+	max_level = levels.size()
