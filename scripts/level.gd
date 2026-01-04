461e697fe905f76fec13503b70b9b4299d9e0af4
amirmohammad ahmadpour <58068218+amap1380@users.noreply.github.com>
2026-01-04 23:40:50 +0330

    added score system ui

---
+++ scripts/level.gd
@@ -0,0 +1,7 @@
+extends Node2D
+class_name Level
+
+@export var number_of_orbs: int = 1
+
+@export var max_positive: int
+@export var max_negetive: int
---
+++ scripts/orbs_container.gd
@@ -0,0 +1,26 @@
+extends HBoxContainer
+
+var positive_index:int = 0
+var negetive_index:int = -1
+
+func set_positive(value: int) -> void:
+	print(value)
+	if positive_index >= get_child_count():
+		return
+	var orb:OrbsUI = get_child(positive_index)
+	if value > orb.max_value * (positive_index + 1):
+		positive_index += 1
+		set_positive(value - int(orb.max_value) * (positive_index))
+	else:
+		print(value)
+		orb.value = value
+
+func set_negetive(value: int) -> void:
+	if negetive_index >= get_child_count():
+		return
+	var orb:OrbsUI = get_child(negetive_index)
+	if value > orb.negetive_bar.max_value * abs(negetive_index):
+		negetive_index -= 1
+		set_positive(value - int(orb.negetive_bar.max_value) * (negetive_index))
+	else:
+		orb.negetive_bar.value = value
